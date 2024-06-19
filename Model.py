fb8ca8a613ca1695d7ad1bf9a5b29f569d60da3f
Derek Smith <dkbsmith03@gmail.com>
2024-06-18 23:33:35 -0400

    init attn

--- Model.py
+++ Model.py
@@ -4,3 +4,28 @@ import torch.nn.functional as F
 import math
 
 
+
+def lrelu(x):
+    pass
+
+
+class LayerNorm(nn.Module):
+    def __init__(self):
+        super(LayerNorm,self).__init__()
+    def forward(self, x):
+        pass
+
+
+class Proj_1d(nn.Module):
+    def __init__(self):
+        super(Proj_1d,self).__init__()
+    def forward(self, x):
+        pass
+
+
+class Attn(nn.Module):
+    def __init__(self):
+        super(Attn,self).__init__()
+    def forward(self, x):
+        pass
+
