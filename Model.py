00663aa5112ae12d7f235ed36355c32c3af64f59
Derek Smith <dkbsmith03@gmail.com>
2024-07-28 12:59:45 -0400

    module model

--- Model.py
+++ Model.py
@@ -2,6 +2,7 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F 
 import math
+import copy
 
 
 
@@ -108,12 +109,11 @@ class FFN_Experts(nn.Module):
         k_outs = torch.gather(outs, 1, idx.unsqueeze(-1).expand(-1, -1, -1, outs.size(-1)))  # Shape: (B, N, k, expert_dim)
         k_vals = vals.unsqueeze(1).unsqueeze(-1).expand(-1,N,-1,k_outs.size(-1))
         out = (k_vals * k_outs).sum(dim=2)
-        return out
+        return out # B,N,D
         
 
-
 class Block(nn.Module):
-    def __inti__(self, config):
+    def __init__(self, config):
         super(Block, self).__init__()
         self.EXP = FFN_Experts(config,config.n_exp)
         self.ATTN = Attn(config)
@@ -126,6 +126,51 @@ class Block(nn.Module):
         x = self.ln_2(m+x)
 
 
-class Model(nn.Module):
+class GPT2Model(nn.Module):
     def __init__(self, config):
-        super(Model, self).__init__()
+        super(GPT2Model, self).__init__()
+        self.n_layer = config.n_layer
+        self.n_embd = config.n_embd
+        self.n_vocab = config.vocab_size
+
+        self.wte = nn.Embedding(config.vocab_size, config.n_embd) # learnable weight matrix that can contain all embeddings
+        self.wpe = nn.Embedding(config.n_positions, config.n_embd) # weights like x
+        block = Block(config.n_ctx, config, scale=True)
+        self.h = nn.ModuleList([copy.deepcopy(block) for _ in range(config.n_layer)]) # layers of blocks
+        self.ln_f = LayerNorm(config.n_embd, eps=config.layer_norm_epsilon)
+
+    def set_embeddings_weights(self, model_embeddings_weights):
+        embed_shape = model_embeddings_weights.shape
+        self.decoder = nn.Linear(embed_shape[1], embed_shape[0], bias=False)
+        self.decoder.weight = model_embeddings_weights  # Tied weights
+
+    def forward(self, input_ids, position_ids=None):
+        if past is None:
+            past_length = 0
+            past = [None] * len(self.h)
+        else:
+            past_length = past[0][0].size(-2)
+        if position_ids is None:
+            position_ids = torch.arange(past_length, input_ids.size(-1) + past_length, dtype=torch.long,
+                                        device=input_ids.device)
+            position_ids = position_ids.unsqueeze(0).expand_as(input_ids)
+
+        input_shape = input_ids.size()
+        input_ids = input_ids.view(-1, input_ids.size(-1))
+        position_ids = position_ids.view(-1, position_ids.size(-1))
+
+        inputs_embeds = self.wte(input_ids)
+        position_embeds = self.wpe(position_ids)
+        if token_type_ids is not None:
+            token_type_ids = token_type_ids.view(-1, token_type_ids.size(-1))
+            token_type_embeds = self.wte(token_type_ids)
+        else:
+            token_type_embeds = 0
+        hidden_states = inputs_embeds + position_embeds + token_type_embeds
+        presents = []
+        for block, layer_past in zip(self.h, past):
+            hidden_states, present = block(hidden_states, layer_past)
+            presents.append(present)
+        hidden_states = self.ln_f(hidden_states)
+        output_shape = input_shape + (hidden_states.size(-1),)
+        return hidden_states.view(*output_shape), presents
