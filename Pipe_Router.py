c92bab9d76ee5baaefdbb5be8d57446c3cbe536d
Derek Smith <dkbsmith03@gmail.com>
2024-11-25 22:55:53 -0500

    finished basic client structure

--- Pipe_Router.py
+++ Pipe_Router.py
@@ -29,11 +29,15 @@ def encode(data, flag, dest_id):
 
 def decode(byte_data):
     buffer = io.BytesIO(byte_data)
+    buffer.seek(0)
     tensor, flag, dest_id = torch.load(buffer)
     return tensor, flag, dest_id
 
 
-def forward_thread_func():
+
+def forward_thread_func(): # thread 
+    print("Forward ON")
+    
     while True:
         try:
             data, flag, dest_id = forward_queue.get() #mb, 0, target_client
@@ -41,12 +45,14 @@ def forward_thread_func():
             message_bytes = encode(data,flag,dest_id)
             dest_bytes = str(dest_id).encode('utf-8')
             router.send_multipart([dest_bytes, message_bytes])
-            print("Sent 0 !!!!!")
+            print(f"sent {data}")
         except queue.Empty:
             pass
 
 
-def backward_thread_func():
+def backward_thread_func(): # thread
+    print("Backprop ON")
+    
     while True:
         try:
             data, flag, dest_id = backward_queue.get() 
@@ -58,55 +64,61 @@ def backward_thread_func():
             pass
 
 
-def listen_thread_func():
+def listen_thread_func(): #thread
+    print("Listener ON")
+    
     i=0
     j=0
     while True:
-        received_bytes = router.recv_multipart()
-        data,flag,dest_id = decode(received_bytes)
+        try:
+            received_bytes = router.recv_multipart()[0]
+            data,flag,dest_id = decode(received_bytes)
+            print(flag)
+            if flag == 0:
+                print("recived 0")
+                if dest_id < num_clients: # client_id is 1 idx, recived from client 4 : dest = 4 and needs reversal 
+                    dest_id += 1 # i.e. if recived from 2, prep for sending to 3
+                    forward_queue.put((data, flag, dest_id)) #keep forwarding
+                if dest_id >= num_clients: # after recived from final client
+                    target = target_queue.get()
+                    loss = Loss_Func(data, target)
+                    loss = loss / batch_ratio # scale loss to keep grads consitent 
+                    flag = 1 #set to backward flag for next round / dont flip cause we sending back to last client frfr
+                    backward_queue.put((loss, flag, dest_id))
 
-        if flag == 0:
-            print("recived 0")
-            if dest_id < num_clients: # client_id is 1 idx, recived from client 4 : dest = 4 and needs reversal 
-                dest_id += 1 # if recived form 2, prep for sending to 3
-                forward_queue.put((data, flag, dest_id)) #keep forwarding
-            if dest_id >= num_clients: # after recived from final client
-                target = target_queue.get()
-                loss = Loss_Func(data, target)
-                loss = loss / batch_ratio # scale loss to keep grads consitent 
-                flag = 1 #set to backward flag for next round / dont flip cause we sending back to last client frfr
-                backward_queue.put((loss, flag, dest_id))
+            if flag == 1:
+                print("Recived 1")
+                if dest_id > 1:
+                    dest_id -= 1
+                    backward_queue.put((data, flag, dest_id))
+                if dest_id <= 1:
+                    i+=1
+                    print(i, end=" ") # mb done
 
-        if flag == 1:
-            print("Recived 1")
-            if dest_id > 1:
-                dest_id -= 1
-                backward_queue.put((data, flag, dest_id))
-            if dest_id <= 1:
-                i+=1
-                print(i, end=" ") # mb done
+            if flag == 2: # pass shell optim.step token through forward so it lines up 
+                if dest_id < num_clients: 
+                    dest_id += 1 
+                    forward_queue.put((data, flag, dest_id))
+                if dest_id >= num_clients: # after recived from final client
+                    flag = 3 # set to real optim.step flag
+                    backward_queue.put((loss, flag, dest_id))
 
-        if flag == 2: # pass shell optim.step token through forward so it lines up 
-            if dest_id < num_clients: 
-                dest_id += 1 
-                forward_queue.put((data, flag, dest_id))
-            if dest_id >= num_clients: # after recived from final client
-                flag = 3 # set to real optim.step flag
-                backward_queue.put((loss, flag, dest_id))
-
-        if flag == 3: # pass grad update message back after last mb
-            if dest_id > 1:
-                dest_id -= 1
-                backward_queue.put((data, flag, dest_id))
-            if dest_id <= 1:
-                j+=1
-                next_batch_event.set()
-                print(f"\nBATCH: {j}\n") # end of Batch
+            if flag == 3: # pass grad update message back after last mb
+                if dest_id > 1:
+                    dest_id -= 1
+                    backward_queue.put((data, flag, dest_id))
+                if dest_id <= 1:
+                    j+=1
+                    next_batch_event.set()
+                    print(f"\nBATCH: {j}\n") # end of Batch
+        except zmq.Again:
+            pass
+        
             
 
-def dataloader_thread_func():
+def dataloader_thread_func(batch_size,micro_batch_size,dataloader,target_queue,forward_queue,next_batch_event): #  multiprocessing
+    print("Dataloader ON")
     i=1
-    print("loading")
     for microbatch in dataloader:
         i+=1
         if i % (batch_size/micro_batch_size) != 0: 
@@ -114,6 +126,7 @@ def dataloader_thread_func():
             target_data = (microbatch["labels"])
             target_queue.put(target_data)
             forward_queue.put(init_data)
+            print(f"{i} in queue")
         else:
             update_token = (0,2,1) 
             forward_queue.put(update_token)
@@ -121,65 +134,24 @@ def dataloader_thread_func():
             next_batch_event.clear()
 
 
-### TODO Not complete  // turn to tractable functions and forward 
-### for init loding different model checkpoints # change per model type and ensure layer size match
-def load_checpoint(path): # "gpt_model_checkpoint.pth" 
-    state_dict = torch.load(path) 
-    client_dicts = [{} for _ in range(num_clients)] # for sub_dicts
-
-    ### split into clients
-    for name, param in state_dict.items():
-        if name.startswith("transformer.h"):
-
-            layer_num = int(name.split(".")[2])  # Get layer number
-            client_id = layer_num // config.n_layer  # Determine client id
-            client_dicts[client_id][name] = param
-        else:
-            for client_dict in client_dicts:
-                client_dict[name] = param
-
-
-
-    ### combine q,k,v proj
-    for client_dict in client_dicts: ### each client dict 
-        q_large, k_large, v_large = [],[],[] 
-        for key,value in client_dict.items(): # add each 
-            if key.endswith("attn.attention.q_proj.weight"):
-                q_large.append(key)
-            
-
-
-    ### itt and print for comp
-    for i,client_dict in enumerate(client_dicts):
-        
-        for key,value in client_dict.items():
-            key=key.replace("transformer.",'')
-            print(key, "     ", list(value.shape))
-            print("\n", end="")
-        
-        breakpoint()
-
-
-        dest_bytes = str(i).encode('utf-8')
-        dict_bytes = encode(client_dict,4,i)
-        router.send_multipart([dest_bytes,dict_bytes])
-
-
 
 if __name__ == "__main__":
 
-    num_clients = 4
-    batch_size = 32
-    micro_batch_size = 8
+    multiprocessing.set_start_method("spawn")
+    
+    num_clients = 2
+    batch_size = 2
+    micro_batch_size = 1
     batch_ratio = batch_size/micro_batch_size
     assert batch_size % micro_batch_size == 0
 
     context = zmq.Context()
     router = context.socket(zmq.ROUTER)
     router.bind("tcp://127.0.0.1:5555")
+    print("Server Running ... ")
 
     data = load_dataset("Skylion007/openwebtext") 
-    debug_data = data['train'].train_test_split(test_size=0.001)['test']
+    debug_data = data['train'].train_test_split(test_size=0.00001)['test']
     tokenizer = GPT2TokenizerFast.from_pretrained("EleutherAI/gpt-neo-1.3B") #(loaded weights specific)
     tokenizer.pad_token = tokenizer.eos_token
     Loss_Func = nn.CrossEntropyLoss()
@@ -190,24 +162,29 @@ if __name__ == "__main__":
     dataset = OpenWebText(config,tokenizer,tokenized_data)
     dataloader = DataLoader(dataset, batch_size=micro_batch_size)
 
-    forward_queue = queue.Queue()
-    backward_queue = queue.Queue()
-    target_queue = queue.Queue()
-    next_batch_event = threading.Event()
-    thread_forward = threading.Thread(target=forward_thread_func)
-    thread_backward = threading.Thread(target=backward_thread_func)
-    thread_listen = threading.Thread(target=listen_thread_func)
-    thread_dataloader = threading.Thread(target=dataloader_thread_func)
 
 
+    manager = multiprocessing.Manager() 
+    
+    forward_queue = manager.Queue()
+    backward_queue = manager.Queue()
+    target_queue = manager.Queue()
+    next_batch_event = manager.Event()
 
-    print("Press Enter when all clients connected")
-    _ = input()
+    thread_forward = threading.Thread(target=forward_thread_func, args=())
+    thread_backward = threading.Thread(target=backward_thread_func, args=())
+    thread_listen = threading.Thread(target=listen_thread_func, args=()) #num_clients,batch_ratio,Loss_Func,target_queue,forward_queue,next_batch_event
+    thread_dataloader = multiprocessing.Process(target=dataloader_thread_func, args=(batch_size,micro_batch_size,dataloader,target_queue,forward_queue,next_batch_event))
+
+
     print("Running...")
-
-
     thread_dataloader.start()
-    thread_forward.start()
-    thread_listen.start()
     thread_backward.start()
-    print("threadin")
+    thread_listen.start()
+    thread_forward.start()
+
+
+    thread_dataloader.join()
+    thread_forward.join()
+    thread_listen.join()
+    thread_backward.join()
--- Pipe_Worker.py
+++ Pipe_Worker.py
@@ -4,6 +4,8 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 import torch.optim as optim
+import queue
+import threading
 from Model_config import Config
 from Clients import Client1
 
@@ -22,51 +24,117 @@ def decode(byte_data):
     return tensor, flag, dest_id
 
 
+def ping():
+    print("pinging server ...")
+    ping = encode("1", 0,0)
+    worker.send_multipart([ping])
+    while True:
+        try:
+            ping_bytes = worker.recv_multipart()[0]
+            print("server response recived")
+            break
+        except zmq.Again:
+            pass
+
+
+def forward_pass(tensor):
+    output = Model(tensor)
+    output.ratain_grad()
+    forward_queue.put()
+    return output
+
+    
+def backward_pass(grad, past_output):
+    past_output.backward(gradient=grad)
+    return past_output.grad
+
+
+def forward_stream():
+    print("Trying forward")
+    forward_stream = torch.cuda.Stream()
+    with torch.cuda.stream(forward_stream):
+        print("Forward started")
+        while True:
+          tensor = forward_queue.get()
+          output = forward_pass(tensor)
+          send_queue.put(output)
+
+
+def backward_stream():
+    print("Trying backward")
+    backward_stream = torch.cuda.Stream()
+    with torch.cuda.stream(backward_stream):
+        print("Backward started")
+        while True:
+          print("looped")
+          grad = backward_queue.get()
+          past_output = output_queue.get()
+          backward_pass(grad,past_output)
+    
+
+def port(): # on cpu
+    print("Running")
+    while True:
+        try:
+            byte_data = worker.recv_multipart(flags=zmq.NOBLOCK)[0]
+            tensor,flag,dest_id = decode(byte_data)
+
+            if flag == 0:
+                print("recived0")
+                forward_queue.put(tensor)
+            
+            if flag == 1:
+                print("recived1")
+                backward_queue.put(tensor)
+
+
+            if flag == 2:
+                print("recived2")
+                send_queue.put(tensor)
+            if flag == 3:
+                print("recived3")
+                backward_queue.put(tensor)
+            
+            out_tensor = send_queue.get()
+            print("sending...")
+            message = encode(out_tensor,flag,dest_id)
+            worker.send_multipart([message])
+
+
+        except zmq.Again:
+            pass    
+
+###############################################################################################################################################
+###############################################################################################################################################
+
 context = zmq.Context()
 worker = context.socket(zmq.DEALER)
 worker.setsockopt(zmq.IDENTITY, b"1")
-worker.connect("tcp://127.0.0.1:5555") ###change to tunnel
+worker.connect("tcp://2.tcp.ngrok.io:18797") ###change to tunnel
 config = Config()
 Model = Client1(config)
 optimizer = optim.Adam(Model.parameters(), lr=0.001)
 
-# state_dict = Model.state_dict()
-# for key, value in state_dict.items():
-#     print(key, "     ", list(value.shape))
-#     print("\n", end="")
-        
-# breakpoint()
+forward_queue = queue.Queue()
+backward_queue = queue.Queue()
+output_queue = queue.Queue()
+send_queue = queue.Queue()
 
-print("running")
-while True:
-    try:
-        byte_data = worker.recv_multipart()[0] ### dont technically need multipart send [0] is workaround 
-        tensor,flag,dest_id = decode(byte_data)
+port = threading.Thread(target=port)
+forward_stream = threading.Thread(target=forward_stream)
+backward_stream = threading.Thread(target=backward_stream)
 
-        if flag == 0:
-            print("recived0")
-            out = Model(tensor) 
-            print("done pass")
-        if flag == 1:
-            print("recived1")
-            tensor.backwards()
 
-        if flag == 2:
-            print("recived2")
-            pass #shuttle data to back pass
-        if flag == 3:
-            print("recived3")
-            optimizer.step()
-            optimizer.zero_grad()
+ping()
 
-        if flag == 4: ### TODO for loading checkpoints
-            state_dict = tensor
-            Model.load_state_dict(state_dict,strict=False)
+port.start()
+forward_stream.start()
+backward_stream.start()
 
-        message = encode(out,flag,dest_id)
-        breakpoint()
-        worker.send_multipart([message])
-        print("sent")
 
-    except zmq.Again:
-        pass
+
+# # Open the browser console (F12), then run:
+# function ClickConnect(){
+#     document.querySelector("colab-toolbar-button#connect").click()
+# }
+# setInterval(ClickConnect, 60000);
